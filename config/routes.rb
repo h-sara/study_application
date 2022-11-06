ecb94c88f053c2392521852f9ac24aa72bc9c1bc
h-sara <115357536+h-sara@users.noreply.github.com>
2022-11-06 08:35:27 +0000

    [Finish] 4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create' #データを追加(保存)するためのルーティング
   get 'lists' => 'lists#index'
-  get 'lists/show'
-  get 'lists/edit'
+  get 'lists/:id' => 'lists#show', as: 'list' # .../lists/1 や.../lists/3 に該当する
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
