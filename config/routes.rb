47dd3ba85fb8e2e3ceb8550d4c8b183ccdfc0d07
h-sara <115357536+h-sara@users.noreply.github.com>
2022-11-07 10:16:14 +0000

    [Finish] 4-5, 4-6, 4-7, 4-8, 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create' #データを追加(保存)するためのルーティング
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list' # .../lists/1 や.../lists/3 に該当する
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # 訳:このファイル内で利用可能なDSLの詳細については、https://guides.rubyonrails.org/routing.html を参照してください。
+  get 'top' => 'homes#top'
+  resources :lists
 end
